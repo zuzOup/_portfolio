ebae746efc40f66435c5e88283c5823f15ad9f8b
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-06-15 12:50:27 -0400

    add bounce dot

--- src/Home/Title/Title.jsx
+++ src/Home/Title/Title.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+
 import "./Title.css";
+import "./Title_bounce.css";
 
 import Carousel from "./Carousel";
 import GetInTouch from "../../Components/GetInTouch";
 
 function Title() {
+  const [animationStart, setAnimationStart] = useState({ dot: "", em1: "", em2: "" });
+
+  useEffect(() => {
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    async function runAnimations() {
+      await delay(300);
+      setAnimationStart((x) => ({ ...x, dot: "animate" }));
+
+      await delay(1000);
+      setAnimationStart((x) => ({ ...x, em1: "animate" }));
+      await delay(620);
+      setAnimationStart((x) => ({ ...x, em2: "animate" }));
+    }
+
+    runAnimations();
+  }, []);
+
   return (
     <section id="title">
       <h3>Hi, my name is</h3>
       <h1>
-        Zuzana Oupická<span className="dot_i"></span>
+        <em className={animationStart.em1}>Zuzana</em>{" "}
+        <em className={animationStart.em2}>Oupická</em>
+        <span id="dot_i" className={animationStart.dot}></span>
       </h1>
       <p>
         I&apos;m a <b>front-end developer</b> located in Canada, exploring the initial
