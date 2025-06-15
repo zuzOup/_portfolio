a3d80e65b51dfe3d23001e0df5fee043b506a132
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-06-15 15:25:34 -0400

    add bounce when dot hits title

--- src/Home/Title/Title.jsx
+++ src/Home/Title/Title.jsx
@@ -7,7 +7,7 @@ import Carousel from "./Carousel";
 import GetInTouch from "../../Components/GetInTouch";
 
 function Title() {
-  const [animationStart, setAnimationStart] = useState({ dot: "", em1: "", em2: "" });
+  const [animationStart, setAnimationStart] = useState({ dot: "", Z: "", O: "" });
 
   useEffect(() => {
     const delay = (ms) => new Promise((res) => setTimeout(res, ms));
@@ -15,11 +15,10 @@ function Title() {
     async function runAnimations() {
       await delay(300);
       setAnimationStart((x) => ({ ...x, dot: "animate" }));
-
       await delay(1000);
-      setAnimationStart((x) => ({ ...x, em1: "animate" }));
+      setAnimationStart((x) => ({ ...x, Z: "animate" }));
       await delay(620);
-      setAnimationStart((x) => ({ ...x, em2: "animate" }));
+      setAnimationStart((x) => ({ ...x, O: "animate" }));
     }
 
     runAnimations();
@@ -28,11 +27,14 @@ function Title() {
   return (
     <section id="title">
       <h3>Hi, my name is</h3>
-      <h1>
-        <em className={animationStart.em1}>Zuzana</em>{" "}
-        <em className={animationStart.em2}>Oupická</em>
-        <span id="dot_i" className={animationStart.dot}></span>
-      </h1>
+      <div className="h1-container">
+        <h1 className={animationStart.Z}>Zuzana</h1>
+        <h1>&nbsp;</h1>
+        <h1 className={animationStart.O}>Oupická</h1>
+        <div id="dot_i_container" className={animationStart.dot}>
+          <div id="dot_i"></div>
+        </div>
+      </div>
       <p>
         I&apos;m a <b>front-end developer</b> located in Canada, exploring the initial
         phases of my career path. I&apos;m making progress on a daily basis as I&apos;m
