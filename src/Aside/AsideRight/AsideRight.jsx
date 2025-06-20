d49720af07b0b57deff08775da2d144c0a097eee
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-06-20 14:53:51 -0400

    email hover wave

--- src/Aside/AsideRight/AsideRight.jsx
+++ src/Aside/AsideRight/AsideRight.jsx
@@ -1,8 +1,35 @@
+import { useRef, useState } from "react";
+
 function AsideRight() {
+  const [animating, setAnimating] = useState(false);
+
+  const timeoutRef = useRef(null);
+
+  const mail = "zuzanaoupicka@gmail.com";
+
+  const triggerAnimation = async () => {
+    if (animating) return; // Prevent stacking
+
+    setAnimating(true);
+
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = setTimeout(() => {
+      setAnimating(false);
+    }, 1700);
+  };
+
   return (
     <div id="aside_right">
-      <a href="mailto:zuzanaoupicka@gmail.com" target="_blank">
-        zuzanaoupicka@gmail.com
+      <a
+        href="mailto:zuzanaoupicka@gmail.com"
+        target="_blank"
+        onMouseEnter={triggerAnimation}
+        className={`${animating ? "animate" : ""}`}
+      >
+        {mail.split("").map((x, i) => (
+          <span key={i}>{x}</span>
+        ))}
       </a>
     </div>
   );
