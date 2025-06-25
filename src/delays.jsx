170a0b99199251c279829cd7c452c44aa4c20152
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-06-25 11:43:20 -0400

    delay times

--- src/delays.jsx
+++ src/delays.jsx
@@ -1,7 +1,18 @@
+//  ---#title---
+//  h3        100ms
+//  h1        250ms
+//  p         400ms
+//  #carousel 550ms
+//  butt      700ms
+
 export const delay_nav = 900;
+
+//☀️        delay_nav           900ms
+//About     delay_nav + 100ms   1000ms
+//Project   delay_nav + 200ms   1100ms
+//Contact   delay_nav + 300ms   1200ms
+//Resume    delay_nav + 400ms   1300ms
 
 export const delay_aside = 1400;
 
 export const delay_bounce = 1600;
-
-
